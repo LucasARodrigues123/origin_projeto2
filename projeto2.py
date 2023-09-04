1a2c5f4596ec9fc77b9f67ffb69a7f5ac74b5958
LucasARodrigues123 <esscur1dao@gmail.com>
2023-09-03 22:57:58 -0300

    Crie A class

---
+++ projeto2.py
@@ -0,0 +1,33 @@
+# criar uma class pessoa e seus atributos 
+class Pessoas:
+    def __init__(self, Nome, Altura, Pesso, Idade,):
+        
+        self.Nome = Nome
+        self.Altura = Altura
+        self.Idade = Idade
+        self.Pesso = Pesso
+    
+# verificar o imc dos Objetos
+    def imc(self):
+        return self.Pesso / (self.Altura ** 2) 
+    
+   
+# verificar A idade
+
+# criar A lista de Objetos pessoas
+
+# criar suas Apresentaçoes com for ou whill
+
+# criar Um Obejeto Usoario
+        
+# criar Espresoes Regulares e Previnir Bugs
+
+
+        
+        
+        
+        
+            
+     
+               
+        
