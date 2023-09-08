5c7a751d1587def3222aa4d864bdc2818d4a5555
LucasARodrigues123 <esscur1dao@gmail.com>
2023-09-08 11:43:53 -0300

    adicionado condisoes if else

--- projeto2.py
+++ projeto2.py
@@ -7,14 +7,34 @@ class Pessoas:
         self.Idade = Idade
         self.Pesso = Pesso
     
-# verificar o imc dos Objetos
-    def imc(self):
-        return self.Pesso / (self.Altura ** 2) 
-    
-   
 # verificar A idade
+def verificarIdade(self):
+    return self.Idade >= 18
 
+# verificar o imc dos Objetos
+def Imc(self):
+    return self.Pesso / (self.Altura ** 2)
+
+# criar ImcLarge
+def ImcLarge(self):
+    Imc = self.imc()
+    if Imc < 18.5:
+        return 'Abaixo do peso'
+    elif Imc < 25.0:
+        return 'Peso normal'
+    elif Imc < 29.0:
+        return 'Exesso de peso'
+    elif Imc < 34.0:
+        return 'Obesidade clase I'
+    elif Imc < 40.0:
+        return 'Obesidade clase II'
+    else:
+        return 'Obesidade classe III'
+def
+    
+    
 # criar A lista de Objetos pessoas
+
 
 # criar suas Apresentaçoes com for ou whill
 
